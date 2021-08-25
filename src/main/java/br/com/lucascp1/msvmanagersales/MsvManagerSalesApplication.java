45669532e762d442b3abb55231e90019d12bfdf2
Lucas Pulcino <lucas.pulcino@VALEMOBI.CORP@VMNOTE-226.valemobi.corp>
2021-08-25 16:02:47 -0300

    Initial Commit

---
+++ src/main/java/br/com/lucascp1/msvmanagersales/MsvManagerSalesApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucascp1.msvmanagersales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvManagerSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvManagerSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucascp1/msvmanagersales/MsvManagerSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucascp1.msvmanagersales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvManagerSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
